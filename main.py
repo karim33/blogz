8debca05391e2ea270d8b407fcb4f32a06faaea3
Abdulkerim <abdulkerim333@gmail.com>
2017-07-16 21:19:54 -0700

    added colab

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, redirect, render_template, session
 from flask_sqlalchemy import SQLAlchemy
+# colab with Osman Abate
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
